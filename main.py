dcf2374d619fdad8ee46f5eb890cef64bac19fe0
pashok <pashok@umich.edu>
2021-10-15 16:27:44 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '!ping':
+        await message.channel.send('Pong!')
+
+client.run(os.environ['TOKEN'])
